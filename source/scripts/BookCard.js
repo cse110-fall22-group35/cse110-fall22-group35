f77faddfa5b19cdc80efba2b07eaca6fee989473
Sarah Thomson <stthomso@ucsd.edu>
2022-11-21 09:31:00 -0800

    made css consistent

--- source/scripts/BookCard.js
+++ source/scripts/BookCard.js
@@ -33,7 +33,7 @@ class BookCard extends HTMLElement {
   }
 
   article{
-      height: 20%;
+      height: 10%;
       width: 13%;
       min-width: 250px;
       min-height: 460px;
@@ -97,7 +97,6 @@ class BookCard extends HTMLElement {
    *                        {
    *                          "imgSrc": "string",
    *                          "imgAlt": "string",
-   *                          "profile": "string",
    *                          "titleTxt": "string",
    *                          "author": "string",
    *                          "bookType": "string"
@@ -121,7 +120,7 @@ class BookCard extends HTMLElement {
     <div class="book_info">
       <span><b><h3>${data.titleTxt}<h3></b></span>
       <h4>${data.author}</h4>
-      <h5>${data.bookType}</h3>
+      <h5>${data.bookType}</h5>
       <div class= "star"></div>
     </div>`;
   }
