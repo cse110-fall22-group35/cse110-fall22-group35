87a7d5687ac8513285de04f3d30adfe2594b13b8
trnick <nlt002@ucsd.edu>
2022-11-22 19:40:47 -0800

    added scripts to rec page

--- source/javascript/recommendations.js
+++ source/javascript/recommendations.js
@@ -4,6 +4,8 @@ genre.addEventListener('click', (event) => {
   genreValue = genre.innerText;
 });
 
+console.log(genre);
+
 const rating = document.getElementById('ratings');
 let ratingStar;
 genre.addEventListener('click', (event) => {
@@ -11,3 +13,9 @@ genre.addEventListener('click', (event) => {
 });
 
 const age = document.querySelector('age').value;
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', (event) => {
+  
+});
