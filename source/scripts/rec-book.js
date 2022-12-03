e69cc7366e1d5055519a3aa9d29f53102607873e
nlt002 <nlt002@ucsd.edu>
2022-12-03 06:19:46 -0800

    updated recommendations page to work with new bookstorage

--- source/scripts/rec-book.js
+++ source/scripts/rec-book.js
@@ -1,52 +1,66 @@
-// rec-book.js
+// recommend-book.js
+
+// importing books from bookstorage.js
+import { booksdb } from '../scripts/bookstorage.js';
+
+    let bookStorage = JSON.stringify(booksdb);
+    let bookToDisplay = bookStorage;
 
 // Run the init() function when the page has loaded
 window.addEventListener('DOMContentLoaded', init);
 
-// Starts the program, all function calls trace back here
+//variables that will be used in the "Recommend Feature"
 let genre = '';
 let ratings = '';
 let price = '';
 let author = '';
+
+// Starts the program, all function calls trace back here
 function init () {
+
   const items = document.querySelectorAll('#genre li');
   // Changes values of certain genres that do not fit in the given cards
   for (var i = 0; i < items.length; i++) {
     items[i].onclick = function () {
       if (this.innerText == 'CHILDREN') {
         document.getElementById('genrebar').value = 'CHILDREN AND TEENS';
-        genre = 'CHILDREN AND TEENS';
+        genre = 'Children and Teens';
       } else if (this.innerText == 'LITERATURE') {
         document.getElementById('genrebar').value = 'LITERATURE AND FICTION';
-        genre = 'LITERATURE AND FICTION';
+        genre = 'Literature and Fiction';
       } else if (this.innerText == 'BIOGRAPHIES') {
         document.getElementById('genrebar').value = 'BIOGRAPHIES AND AUTO BIOGRAPHIES';
-        genre = 'BIOGRAPHIES AND AUTO BIOGRAPHIES';
+        genre = 'Biographies and Auto Biographies';
       } else if (this.innerText == 'ACADEMIC') {
         document.getElementById('genrebar').value = 'ACADEMIC AND PROFESSIONAL';
-        genre = 'ACADEMIC AND PROFESSIONAL';
+        genre = 'Academic and Professional';
       } else if (this.innerText == 'REGIONAL') {
         document.getElementById('genrebar').value = 'REGIONAL BOOKS';
-        genre = 'REGIONAL BOOKS';
+        genre = 'Regional Books';
       } else if (this.innerText == 'BUSINESS') {
         document.getElementById('genrebar').value = 'BUSINESS AND MANAGEMENT';
-        genre = 'BUSINESS AND MANAGEMENT';
+        genre = 'Business and Management';
       } else if (this.innerText == 'HEALTH') {
         document.getElementById('genrebar').value = 'HEALTH AND COOKING';
-        genre = 'HEALTH AND BOOKING';
+        genre = 'Health and Cooking';
       } else {
         document.getElementById('genrebar').value = this.innerText;
         genre = this.innerText;
       }
     };
+
   }
+
+  //assigns rating value chosen by user to variable ratings...
   const rate = document.querySelectorAll('#ratings li');
   for (var i = 0; i < rate.length; i++) {
     rate[i].onclick = function () {
-      document.getElementById('placeholder1').innerText = this.innerText;
+      document.getElementById('placeholder1').innerText = this.innerText + " ✩";
       ratings = this.innerText;
     };
   }
+
+  //assigns price value chosen by user to variable price...
   const prices = document.querySelectorAll('#price li');
   for (var i = 0; i < prices.length; i++) {
     prices[i].onclick = function () {
@@ -54,13 +68,119 @@ function init () {
       price = this.innerText;
     };
   }
+  
   // Get the books from localStorage
   const books = getBooksFromStorage();
   // Add each book to the <main> element
   addBooksToDocument(books);
-  // Add the event listeners to the form elements
-  // initFormHandler();
 }
+
+/**
+ * NavBar Reset
+ *
+ * Description:
+ * Resets the local storage when traversing through other sites, since we do not want to have the
+ *          users to continue to view recommended items on the home page.
+ *
+ */
+let navBtn = document.getElementById('nav');
+navBtn.addEventListener('click', function() {
+    const bookStorage = JSON.stringify(booksdb);
+    const bookToDisplay = bookStorage;
+    localStorage.setItem('books', bookToDisplay);
+  
+    const books = getBooksFromStorage();
+    addBooksToDocument(books);
+});
+
+/**
+ * Recommend Feature
+ *
+ * Description:
+ * Displays books that fall into categories chosen in the forms given on the recommendations page:
+ *      1. 'All': display all books in storage;
+ *      2. 'Entrance Exam': display only books with genre 'Entrance Exam';
+ *      3. 'fiction':  display ......                     'Literature and Fiction';
+ *      4. 'academic': display ......                     'Academic and Professional';
+ *      5. 'biograph': display ......                     'Biographies and Auto Biographies';
+ *      6. 'business': display ......                     'Business and Management';
+ * 
+ * NOTE:
+ * 1. Takes three mandatory items to filter/recommend (author is optional and will display all books with that author if inputted)
+ * 1. Executes when search button is clicked on
+ *
+ */
+document.getElementById('myBtn').addEventListener('click', function () {
+
+    bookStorage = JSON.parse(bookStorage);
+
+    author = document.getElementById('author').value.toUpperCase();
+
+    // Use genre choice from filter search bar
+    switch (genre) {
+      case 'ENTRANCE EXAM':
+        bookToDisplay = bookStorage.filter(function (el) {
+          if (author != '') {
+            return (el.Category === 'Entrance Exam' && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+          }
+          return el.Category === 'Entrance Exam' && el.Rating >= ratings && el.Price <= price;
+        });
+      break;
+      case 'Children and Teens':
+            bookToDisplay = bookStorage.filter(function (el) {
+                if (author != '') {
+                    return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+                }
+                return el.Category === genre && el.Rating >= ratings && el.Price <= price;
+            });
+      break;
+      case 'Literature and Fiction':
+            bookToDisplay = bookStorage.filter(function (el) {
+                if (author != '') {
+                    return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+                }
+                return el.Category === genre && el.Rating >= ratings && el.Price <= price;
+            });
+      break;
+      case 'Academic and Professional':
+        bookToDisplay = bookStorage.filter(function (el) {
+            if (author != '') {
+                return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+            }
+          return el.Category === genre && el.Rating >= ratings && el.Price <= price;
+        });
+        break;
+      case 'Biographies and Auto Biographies':
+        bookToDisplay = bookStorage.filter(function (el) {
+            if (author != '') {
+                return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+            }
+          return el.Category === genre && el.Rating >= ratings && el.Price <= price;
+        });
+        break;
+      case 'Business and Management':
+        bookToDisplay = bookStorage.filter(function (el) {
+            if (author != '') {
+                return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+            }
+          return el.Category === genre && el.Rating >= ratings && el.Price >= price;
+        });
+        break;
+       case 'Health and Cooking':
+        bookToDisplay = bookStorage.filter(function (el) {
+            if (author != '') {
+                return (el.Category === genre && el.Rating >= ratings && el.Price <= price) || (el.Author.toUpperCase() == author);
+            }
+            return el.Category === genre && el.Rating >= ratings && el.Price <= price;
+        });
+        break;
+    }
+    
+    bookStorage = JSON.stringify(bookStorage);
+    bookToDisplay = JSON.stringify(bookToDisplay);
+    localStorage.setItem('books', bookToDisplay);
+    init();
+  });
 
 /**
  * Reads 'books' from localStorage and returns an array of
@@ -90,40 +210,19 @@ function getBooksFromStorage () {
  */
 function addBooksToDocument (books) {
   if (!books) return;
-
+  // A10. TODO - Get a reference to the <main> element
   const sectionEl = document.querySelector('section');
+  sectionEl.innerHTML = '';
+  // console.log(mainEl);
+  // A11. TODO - Loop through each of the books in the passed in array,
+  //            create a <book-card> element for each one, and populate
+  //            each <book-card> with that book data using element.data = ...
+  //            Append each element to <main>
+  books.forEach(createBookCard);
 
-  // Event listener to search for filtered books
-  document.getElementById('myBtn').addEventListener('click', function () {
-    // Select element of autohr
-    author = document.getElementById('author').value.toUpperCase();
-    // Loop through each of the books in the passed in array...
-    books.forEach(createBookCard);
-    // Function to create a book card for filtered items
-    function createBookCard (item) {
-      if (item.Category != null && item.Author != null && genre != '') {
-        if (((item.Category.toUpperCase() == genre) && (item.Price <= price)) || item.Author.toUpperCase().includes(author)) {
-          // Create a book-card element
-          const element = document.createElement('book-card');
-          // Populate <book-card> with book data
-          element.data = item;
-          // Append each element to <section>
-          sectionEl.append(element);
-        }
-      }
-    }
-  });
-}
-
-/**
- * Takes in an array of books, converts it to a string, and then
- * saves that string to 'books' in localStorage
- * @param {Array<Object>} books An array of books
- */
-function saveBooksToStorage (books) {
-  // EXPLORE - START (All explore numbers start with B)
-  // B1. TODO - Complete the functionality as described in this function
-  //            header. It is possible in only a single line, but should
-  //            be no more than a few lines.
-  localStorage.setItem('books', JSON.stringify(books));
+  function createBookCard (item) {
+    const element = document.createElement('book-card');
+    element.data = item;
+    sectionEl.append(element);
+  }
 }
