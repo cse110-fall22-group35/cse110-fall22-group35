64b0a2229c289cb3e78f25d431a8b252d430eaff
Qiyun506 <83425380+Qiyun506@users.noreply.github.com>
2022-12-04 00:23:45 -0800

    add the remove function

--- source/scripts/individual-book.js
+++ source/scripts/individual-book.js
@@ -9,6 +9,7 @@ window.addEventListener('DOMContentLoaded', init);
  * All function calls trace back to here, controls the individual-book page's functionality
  */
 function init () {
+  removeBook()
   // Add books information
   updateInfo();
   // getting and adding reviews to the page
@@ -16,7 +17,6 @@ function init () {
   updateRating(reviews);
   reviews.forEach(addCommentToDocument);
   setupDeleteButtons();
-
   /**
    * Adds functionality to the delete buttons for every comment on the page
    */
@@ -255,3 +255,30 @@ function findExistingUser (reviewList, name) {
   }
   return -1;
 }
+
+/**
+ * remove the bookfrom local storage and back to the homepage
+ */
+
+function removeBook(){
+  const btn = document.getElementById("delete");
+  btn.addEventListener('click',(event)=>{
+    console.log('del clicked.');
+    libUpdate(bookTitle);
+  })
+}
+function libUpdate (title) {
+  console.log('clear the book with title');
+  let libraryBook = localStorage.getItem('books');
+  console.log(title);
+  libraryBook = JSON.parse(libraryBook);
+  console.log(libraryBook.length)
+  let afteraction = libraryBook.filter(el => (el.Title).includes(title));
+  console.log(afteraction.length)
+  let bookRemain = JSON.stringify(afteraction);
+  localStorage.setItem('books', bookRemain);
+}
+
+
+
+
